5e567d782f8fc70e4298d15256ca3e94e7546878
yfigueira <figueirapy@gmail.com>
2025-07-02 20:00:42 +0200

    account service:: bug fix on job title mapper and database table

--- account-service/src/main/java/org/example/accountservice/jobtitle/persistence/JobTitleMapper.java
+++ account-service/src/main/java/org/example/accountservice/jobtitle/persistence/JobTitleMapper.java
@@ -2,8 +2,9 @@ package org.example.accountservice.jobtitle.persistence;
 
 import org.example.accountservice.jobtitle.domain.JobTitle;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 interface JobTitleMapper {
 
     JobTitle toDomain(JobTitleEntity entity);
